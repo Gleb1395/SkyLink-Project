d101ba55642e13c03c46807d0b3065348dae1920
ComfortCityBud <gleb@gmail.com>
2025-03-05 14:06:09 +0200

    Added CI/CD

--- src/manage.py
+++ src/manage.py
@@ -8,7 +8,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    if sys.argv[1].lower() =="test":
+    if sys.argv[1].lower() == "test":
         print("NOTE: Running black formatter")
         subprocess.run(["black", "--config", ".black.toml", "."])
         subprocess.run(["isort", "."])
