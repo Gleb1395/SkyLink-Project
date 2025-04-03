bb755440b11f5518b6d348f1d1f2e48a57c5fec0
ComfortCityBud <gleb@gmail.com>
2025-04-03 15:06:24 +0300

    Added postgres DB

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,7 @@ WORKDIR /SkyLink-Project
 
 COPY ./src ./src
 COPY ./requirements.txt ./requirements.txt
-#todo убрать env добавить в compose
-COPY ./.env ./.env
+
 COPY ./commands ./commands
 
 RUN python -m pip install --upgrade & pip install -r ./requirements.txt
--- src/config/settings.py
+++ src/config/settings.py
@@ -84,6 +84,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("POSTGRES_DB"),
+#         "USER": os.getenv("POSTGRES_USER"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#         "HOST": "postgres",
+#         "PORT": "5432",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
